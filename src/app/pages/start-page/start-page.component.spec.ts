dbe15b8513b517fc4102da88522ce20c0b625f86
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-11-12 20:59:01 +0300

    Сделана базовая логика переключения компонентов

--- src/app/pages/start-page/start-page.component.spec.ts
+++ src/app/pages/start-page/start-page.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LoginModule } from 'src/app/modules/login';
+import { RegistrationModule } from 'src/app/modules/registration';
 import { StartPageComponent } from './start-page.component';
 
 describe('StartPageComponent', () => {
@@ -8,6 +11,7 @@ describe('StartPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [StartPageComponent],
+      imports: [LoginModule, RegistrationModule, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StartPageComponent);
--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { MetaDefinition } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { IPage } from './../../shared/models/page';
 
 @Component({
   selector: 'social-empire-start-page',
@@ -7,4 +10,30 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StartPageComponent {}
+export class StartPageComponent implements IPage, OnInit, OnDestroy {
+  title = 'Стартовая страница';
+  header = 'Начальная страница';
+  description = 'Приветственная страница для пользователя';
+  ogDescription?: string;
+  image?: string;
+  robots?: string;
+  meta?: MetaDefinition[];
+  tags?: string | string[];
+  cssModules?: string[];
+  documents?: any[];
+
+  constructor(private readonly router: Router, private readonly _cdf: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.checkUrl();
+  }
+
+  ngOnDestroy(): void {
+    this._cdf.markForCheck();
+  }
+
+  checkUrl() {
+    const routeUrl = this.router.url;
+    return routeUrl;
+  }
+}
