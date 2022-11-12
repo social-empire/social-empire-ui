4b3e351d51f7df54c074e9d964b573009c53f45e
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-11-12 20:59:17 +0300

    Дополнение к предыдущему коммиту

--- src/app/pages/start-page/start-page.module.ts
+++ src/app/pages/start-page/start-page.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { StartPageComponent } from './start-page.component';
 import { LoginModule } from 'src/app/modules/login';
 import { RegistrationModule } from 'src/app/modules/registration';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: StartPageComponent, pathMatch: 'full' }];
 
 @NgModule({
   declarations: [StartPageComponent],
-  imports: [CommonModule, LoginModule, RegistrationModule],
+  imports: [CommonModule, RouterModule.forChild(routes), LoginModule, RegistrationModule],
   exports: [StartPageComponent],
 })
 export class StartPageModule {}
