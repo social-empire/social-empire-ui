f934598291a1f7ae4fe43f0bd61200195067f0da
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-11-12 19:06:52 +0300

    Внесены изменения в ключевой компонент app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageModule } from './pages/start-page';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, StartPageModule],
   providers: [],
   bootstrap: [AppComponent],
   exports: [AppComponent],
