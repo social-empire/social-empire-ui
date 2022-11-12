15657c77432b61bc5a5967b03dce688e1447ca7b
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-11-12 20:58:14 +0300

    Добавлен роутинг

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './modules/registration';
+import { StartPageComponent } from './pages/start-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/start-page',
+    pathMatch: 'full',
+  },
+  {
+    path: 'start-page',
+    component: StartPageComponent,
+    children: [
+      {
+        path: 'registration',
+        component: RegistrationComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
