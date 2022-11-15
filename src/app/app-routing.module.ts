3fb8bccbfa256ed6fc80df0c25bf2081f23740cb
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-11-16 01:47:39 +0300

    Замена в app-routing модуле

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegistrationComponent } from './modules/registration';
-import { StartPageComponent } from './pages/start-page';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: '/start-page',
     pathMatch: 'full',
-  },
-  {
-    path: 'start-page',
-    component: StartPageComponent,
-    children: [
-      {
-        path: 'registration',
-        component: RegistrationComponent,
-      },
-    ],
   },
 ];
 
