f522a4f5a9b8d999a8421c4e1509daa7f54a0998
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2023-01-22 23:32:45 +0300

    Начало разработки формы логина

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -13,7 +14,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), FormsModule, ReactiveFormsModule],
   exports: [LoginComponent],
 })
 export class LoginModule {}
